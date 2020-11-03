02f0a461cb61f69a50da09ef224feac8cc99d1f6
Vincent He <vincent0710@Outlook.com>
2020-11-03 14:36:09 -0500

    Initial Commit

---
+++ WeSplitBO/WeSplitBOApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitBOApp.swift
+//  WeSplitBO
+//
+//  Created by Vincent He on 2020-11-03.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitBOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
